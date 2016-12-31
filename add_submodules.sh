e7e3629c99209c9f581f1b0883e54fdb53a40743
dchilot <dchilot@hotmail.com>
2016-12-31 14:42:06 +0000

    The idea of sharing dependencies does not work with modules.

--- add_submodules.sh
+++ add_submodules.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 while read -r repo ; do git submodule add --name $repo https://github.com/orwell-int/$repo.git ; done < modules
+
+#git submodule add https://github.com/dchilot/germs.git germs
+#git submodule add https://github.com/google/googletest.git googletest
+#mkdir -p deploy
+#cd deploy
+#ln -s ../capture-video .
+#ln -s ../germs .
+#cd -
+#mkdir -p third_party
+#cd third_party
+#ln -s ../googletest
+#cd -
--- link.sh
+++ link.sh
@@ -7,6 +7,12 @@ for gitmodules in */.gitmodules ; do
 	sed 's/\[submodule "\([^"]\+\)"\]/\1/p' -n .gitmodules
 	for repo in $(sed 's/\[submodule "\([^"]\+\)"\]/\1/p' -n .gitmodules) ; do
 		if [ -e "../$repo" ] ; then
+			# this does not work because it changes the state of the local folder
+			# there is apparently no easy way to make sure the repositories share
+			# the same versions of modules
+			if [ -d "$repo" ] ; then
+				echo rm -rf "$repo"
+			fi
 			echo ln -s ../$repo .
 		else
 			echo "Not found: $repo" >&2
